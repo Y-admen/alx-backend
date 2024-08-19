9d812e1bb3cea51ec4115c4ddc7d41542e0f31cc
Y-admen <yassminash305@gmail.com>
2024-08-19 18:25:59 +0300

    simple_helper_function.3

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,12 +2,21 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int = 1, page_size: int = 0) -> Tuple[int, int]:
     """_summary_
 
     Returns:
         Tuple: start_index, end_index
     """
+    if page < 1 or page_size < 1:
+        raise ValueError("ValueError")
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return start_index, end_index
+res = index_range(1, 7)
+print(type(res))
+print(res)
+
+res = index_range(page=3, page_size=15)
+print(type(res))
+print(res)
