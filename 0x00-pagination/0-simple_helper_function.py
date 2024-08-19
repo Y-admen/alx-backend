4c087feaa7ead07db08ff8c59efc7b8c6620f9f2
Y-admen <yassminash305@gmail.com>
2024-08-19 18:13:02 +0300

    simple_helper_function.1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -14,4 +14,4 @@ def index_range(page: int, page_size: int) -> Tuple:
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    return (start_index, end_index)
+    return start_index, end_index
