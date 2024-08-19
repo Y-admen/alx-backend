a2fcb0389cac16923770766dbb09f6186cd52533
Y-admen <yassminash305@gmail.com>
2024-08-19 18:29:45 +0300

    simple_helper_function.3

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,8 +8,8 @@ def index_range(page: int = 1, page_size: int = 0) -> Tuple[int, int]:
     Returns:
         Tuple: start_index, end_index
     """
-    if page < 1 or page_size < 1:
-        raise ValueError("ValueError")
+    if page < 1:
+        page = 1
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return start_index, end_index
