51cfc26b56b3b993a5fab7ac11dbb59636673715
Y-admen <yassminash305@gmail.com>
2024-08-19 19:47:37 +0300

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -24,7 +24,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Returns the appropriate page of the dataset based on the given page and page_size.
+        """Returns the appropriate page of the dataset based on
+        the given page and page_size.
 
         Args:
             page (int, optional): Page number (default is 1).
@@ -33,11 +34,14 @@ class Server:
         Returns:
             List[List]: Paginated list of rows.
         """
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
+        assert isinstance(page, int) and page > 0, """Page must be a
+        positive integer"""
+        assert isinstance(page_size, int) and page_size > 0, """Page size
+        must be a positive integer"""
 
         start_idx, end_idx = index_range(page, page_size)
         return self.dataset()[start_idx:end_idx]
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Calculates the start and end indexes for pagination.
