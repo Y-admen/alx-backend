df2a737f7010058500d885bcafb16cd17b5e91f1
Y-admen <yassminash305@gmail.com>
2024-08-21 18:56:28 +0300

    task1.1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 " FIFO caching"
+from typing import Optional, Any
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
     "FIFOCache"
-    def __init__(self):
+    def __init__(self)-> None:
         super().__init__()
-        self.key_list = []
+        self.key_list: list[str] = []
 
-    def put(self, key, item):
-        "add item"
+# Optional[X] is a shorthand for Union[X, None]/in str can not be None
+    def put(self, key: Optional[str], item: Optional[Any])-> None:
+        """ Add an item in the cache
+        """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            self.order.remove(key)
+            self.key_list.remove(key)
         elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            first_key = self.order.pop(0)
+            first_key = self.key_list.pop(0)
             del self.cache_data[first_key]
             print(f"DISCARD: {first_key}")
 
         self.cache_data[key] = item
-        self.order.append(key)
+        self.key_list.append(key)
 
-    def get(self, key):
+    def get(self, key: Optional[str])-> Optional[Any]:
         """ Get an item by key """
         return self.cache_data.get(key, None)
