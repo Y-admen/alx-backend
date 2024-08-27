fdd2b5647995152af8f88fde64c5d9368538c6f4
Y-admen <yassminash305@gmail.com>
2024-08-27 22:38:50 +0300

    LANGUAGES

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 "Basic Babel setup"
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -25,6 +25,12 @@ def render_temp():
         templates to render
     """
     return render_template("1-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
